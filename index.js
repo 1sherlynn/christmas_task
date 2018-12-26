18153754ba2f81b49efb435777bab235af511269
sherlynn <1sherlynn@gmail.com>
2018-12-26 11:58:44 +0800

    install mongodb, mongoose and connect to local database

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const app = express()
 var api = require('./api')
+
+var mongoose = require('mongoose');
+var db_url = 'mongodb://localhost:27017/books';
+
+mongoose.connect(db_url);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error: '));
+db.once('open', function() {
+    console.log("Successfully connected to Mongo Server.");
+});
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
 
 const port = 8001
 
