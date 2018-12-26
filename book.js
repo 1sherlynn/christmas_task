21236cdececf25001873d19011bbe767654d0fdb
sherlynn <1sherlynn@gmail.com>
2018-12-26 12:21:51 +0800

    RETURNING TEXT still, added book schema and model

---
+++ book.js
@@ -0,0 +1,13 @@
+let mongoose = require('mongoose')
+let validator = require('validator')
+
+let bookSchema = new mongoose.Schema({
+  title: {
+  	type: String,
+  	required: true
+  }
+  author: String, 
+  year: Number
+})
+
+module.exports = mongoose.model('Book', bookSchema );
